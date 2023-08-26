69a7d6c4f415a826d5449f84bcac0de00d99fdd2
Alvin Li <aflip24@gmail.com>
2023-08-26 01:09:22 -0500

    add option to list all users

--- lib/cli.py
+++ lib/cli.py
@@ -1,19 +1,36 @@
 import click
+import os
+import platform
 from fitnesstracker import FitnessTracker
 
-db_name = 'fitness_tracker.db'
+db = 'fitness_tracker.db'
+
+def clear_console():
+    system = platform.system()
+    if system == "Windows":
+       os.system("cls")
+    else:
+        os.system("clear")
 
 @click.command()
 def main():
+    clear_console()
     print_menu()
 
     while True:
         choice = click.prompt("Select an option", type=int)
 
         if choice == 1:
+            clear_console()
             add_user()
             break
         elif choice == 2:
+            clear_console()
+            log_in()
+            break
+        elif choice == 3:
+            clear_console()
+            list_all_users()
             log_in()
             break
         else:
@@ -25,13 +42,13 @@ def print_menu():
         click.echo(menu_content)
 
 def add_user():
-    tracker = FitnessTracker(db_name)
+    tracker = FitnessTracker(db)
     username = click.prompt("Enter username")
     tracker.add_user(username)
     click.echo(f"Profile {username} was created.")
 
 def log_in():
-    tracker = FitnessTracker(db_name)
+    tracker = FitnessTracker(db)
     username = click.prompt("Enter username")
     user_id = tracker.get_user_id(username)
 
@@ -40,16 +57,27 @@ def log_in():
     else:
         click.echo("Invalid username.")
 
+def list_all_users():
+    tracker = FitnessTracker(db)
+    users = tracker.get_all_users()
+
+    click.echo("Available Profiles:")
+    for user in users:
+        click.echo(user[1])
+
 def user_submenu(username):
     while True:
+        clear_console()
         print_user_submenu(username)
 
         choice = click.prompt("Select an option", type=int)
 
         if choice == 1:
             option1(username)
+            break
         elif choice == 2:
             option2(username)
+            break
         else:
             click.echo("Please select a valid option.")
 
--- lib/fitnesstracker.py
+++ lib/fitnesstracker.py
@@ -19,5 +19,17 @@ class FitnessTracker:
             self.conn.execute('INSERT INTO users (username) VALUES (?)', (username,))
             self.conn.commit()
 
+    def get_user_id(self, username):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT id FROM users WHERE username = ?', (username,))
+        user_id = cursor.fetchone()
+        return user_id[0] if user_id else None
+    
+    def get_all_users(self):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * from users')
+        users = cursor.fetchall()
+        return users
+
 
         
